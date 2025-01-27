9c893f4106f29045e40c34acdabd25594e2e7066
Gustavo Ribeiro <gustavoribeiro0901@gmail.com>
2025-01-27 18:56:21 -0300

    Save data and generate HTML for payment Summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
-import { cart } from "../data/cart.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 
-renderOrderSummary(cart);
+renderOrderSummary();
+renderPaymentSummary();
